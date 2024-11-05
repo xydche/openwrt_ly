323b175ab3bb465f30234b3486f91d24e4aede6d
xydche <cherhe@gmail.com>
2024-11-05 11:23:01 +0800

    恢复luci到低版本

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,9 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+#恢复lede中luci到低版本
+sed -i '2s/^#//' feeds.conf.default
+sed -i '3s/^/#/' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"

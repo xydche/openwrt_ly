70b77aa9f6b605fa549a1167218decc3dddfb537
xydche <cherhe@gmail.com>
2024-11-06 12:41:51 +0800

    添加PW

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,8 +18,8 @@ sed -i '2s/^#//' feeds.conf.default
 sed -i '3s/^/#/' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 # echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 #git clone https://github.com/xydche/luci-theme-design.git package/luci-theme-design
 git clone https://github.com/kenzok78/luci-app-adguardhome.git package/luci-app-adguardhome
